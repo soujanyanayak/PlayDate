080c13eda675ec7fd3b4ba37ec93e76236e5e77b
soujanyanayak18 <snayak@horizon.csueastbay.edu>
2022-06-30 21:34:34 -0700

    created insertpublicevents.py and events search

--- application/PlayDate/events/views.py
+++ application/PlayDate/events/views.py
@@ -1,11 +1,85 @@
+from cgitb import lookup
+from unittest import result
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+from events.models import Event, Location
+
+import requests
+from bs4 import BeautifulSoup
+
 # Create your views here.
 
 
-def events(request):
-    return render(request, "events/events.html")
+# def searchevents(city):
+#     state=''
+
+#     URL = "https://www.eventbrite.com/d/"+state+"--"+city+"/dog-events/"
+    
+#     page = requests.get(URL)
+
+#     soup = BeautifulSoup(page.content, "html.parser")
+
+#     results=soup.find_all("div", {"class": "eds-event-card-content__content"})
+#     events=[]
+#     #print(page.text)
+#     #print("\n\n\n\n\n")
+#     for item in results:
+#         #print(item.prettify())
+#         links = item.find_all('a')
+#         blog_titles = item.findAll('h3', attrs={"class":"eds-event-card-content__title eds-text-color--ui-800 eds-text-bl eds-text-weight--heavy"})
+#         venue = item.findAll("div", {"data-subcontent-key":"location"})
+#         #print(links)
+#         url=''
+#         location=''
+#         for link in links:
+#             if "href" in link.attrs:
+#                 url=link.attrs['href']
+#         for loc in venue:
+#             location=loc.text
+#         for title in blog_titles:
+#             if str(title.text).find('Dog') != -1:
+#                 event={}
+#                 event['title']= str(title.text)
+#                 event['url']=str(url)
+#                 event['location']=str(location)
+#                 event['source']="Eventbrite"
+#                 events.append(event)
+#     return events
 
 
 def createEvent(request):
     return render(request, "events/createEvent.html")
+
+
+def events(request):
+    if request.method == 'GET':
+        query= request.GET.get('q')
+        print(query)
+        submitbutton= request.GET.get('submit')
+
+        if query is not None: 
+            # context={'results': searchevents(query),'submitbutton': submitbutton}
+            #result=Event.objects.select_related('location')
+
+            print(query)
+           
+            lookups= Q(location__city__icontains=query) | Q(location__state_name__icontains=query)
+
+            results= Event.objects.filter(lookups).distinct()
+
+            context={'results': results,
+                     'submitbutton': submitbutton}
+
+            return render(request, 'events/events.html', context)
+
+        else:
+            return render(request, 'events/events.html')
+
+    else:
+        return render(request, 'events/events.html')
+
+
+
+
+
