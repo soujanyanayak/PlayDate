869fb9f0934855448a23c04d8e800a054b37fb98
Qin Geng <qingeng@Qins-MacBook-Pro.local>
2022-07-03 03:07:48 -0700

    update login page and function

--- application/PlayDate/home/views.py
+++ application/PlayDate/home/views.py
@@ -56,6 +56,17 @@ def home(request):
 
 
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    context = {}
     return render(request, 'home/login.html')
 
 
