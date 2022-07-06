4e6a05392c41bcc3f3f72fbf2831ebfcecc927c9
soujanyanayak18 <snayak@horizon.csueastbay.edu>
2022-07-06 15:58:54 -0700

    search working

--- application/PlayDate/events/urls.py
+++ application/PlayDate/events/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.events, name='events'),
     path('createEvent/', views.createEvent, name='eventCreate'),
+    path('filter/', views.filter, name='filter'),
 ]
