0c0b31325f53ed355e2a48a8b76862f619282d1e
Qin Geng <qingeng@Qins-MacBook-Pro.local>
2022-07-07 00:27:50 -0700

    recover the homepage

--- application/PlayDate/playdate/settings.py
+++ application/PlayDate/playdate/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'playdate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'playdatedb',
-        'USER': 'root',
-        'PASSWORD': 'sou@12345',
+        'NAME': 'playdate',
+        'USER': 'qin',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
