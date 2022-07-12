4ba4f3c10e827d87f091359fe38a6bb95ffe02a1
andrewyjo <andrewyunejo@gmail.com>
2022-07-12 15:18:59 -0700

    updated playdate/urls.py

--- application/PlayDate/playdate/urls.py
+++ application/PlayDate/playdate/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -10,6 +11,11 @@ urlpatterns = [
     path('events', include('events.urls')),
     path('about/', include('members.urls')),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # if settings.DEBUG :
 #     urlpatterns += static(settings.STATIC_URL,document_root =settings.STATIC_ROOT)
