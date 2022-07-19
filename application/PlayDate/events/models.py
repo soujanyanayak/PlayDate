7cc4cd76a792930f74ceab67b3f4f9c068aff287
soujanyanayak18 <snayak@horizon.csueastbay.edu>
2022-07-18 20:41:35 -0700

    added banner in models and events

--- application/PlayDate/events/models.py
+++ application/PlayDate/events/models.py
@@ -5,6 +5,7 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
 from groups.models import  Group, Groupadmin
@@ -16,8 +17,9 @@ from home.models import Address, Backendadmin
 class Publicevent(models.Model):
     public_event_id = models.AutoField(primary_key=True)
     address = models.ForeignKey(Address, models.DO_NOTHING)
-    event_url = models.CharField(max_length=1000)
     name = models.CharField(max_length=500)
+
+    banner = models.ImageField(upload_to="publicevents_banner", default=None, blank=True)
     # geometry= geomodels.PointField()
 
     category = models.CharField(max_length=10, blank=True, null=True)
