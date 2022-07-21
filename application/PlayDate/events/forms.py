2b94dadd350c4d30a990f61c48524473b8f5e044
soujanyanayak18 <snayak@horizon.csueastbay.edu>
2022-07-21 15:40:08 -0700

    createEvent

--- application/PlayDate/events/forms.py
+++ application/PlayDate/events/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, widgets
 from events.models import Event, Publicevent
 
 
@@ -16,10 +16,21 @@ class GroupEventForm(forms.Form):
     country = forms.CharField(label='Country', max_length=200)
     zipcode = forms.CharField(label='Zipcode', max_length=200)
 
-
+#public events
 class PublicEventForm(ModelForm):
     class Meta:
         model = Publicevent
         fields = ['address', 'name', 'banner', 'category']
         labels = {'address': ('Address'),
                   'name': ('Event Name'), 'banner': ('banner'), 'category': ('category')}
+
+#user events
+class eventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields=['name', 'banner', 'category', 'desc','datetime','user']
+        labels={'name':('Event Name'),'banner':('Image'),'category':('category'),'desc':('desc'),'datetime':('datetime')}
+        exclude = ()
+        widgets = {
+            'datetime': widgets.DateInput(attrs={'type': 'date'})
+        }
