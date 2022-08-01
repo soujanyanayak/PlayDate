883726a483c0de9e9ba0fe1b325b397f43eb1778
soujanyanayak18 <snayak@horizon.csueastbay.edu>
2022-08-01 15:02:20 -0700

    merged public and user events to memberevents.hmtl

--- application/PlayDate/events/urls.py
+++ application/PlayDate/events/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.events, name='events'),
-    path('public-events/', views.publicevents, name='publicevents'),
+    # path('public-events/', views.publicevents, name='publicevents'),
+    path('public-events/', views.membersevents, name='membersevents'),
     path('members-events/', views.membersevents, name='membersevents'),
     # path('my-events/', views.myevents, name='myevents'),
     # path('my-events/create-user-event/', views.createUserEvent, name='createUserEvent'),
